ab2119bb886246458ebcecffd0bc7a5af6b14916
Nathaniel <nathanielpaulrobinson@gmail.com>
2021-04-15 14:39:04 -0600

    dynamic weighting (direct v. indirect) & (motorized v. non-motorized)

--- src/hii_road.py
+++ src/hii_road.py
@@ -7,11 +7,14 @@ from task_base import HIITask
 class HIIRoad(HIITask):
     SCALE = 100  # TODO: test outputs and export times with 300 vs 100
     OSM_START = datetime(2012, 9, 12).date()
-    KERNEL_DISTANCE = 500
-    DIRECT_INFLUENCE_RADIUS = 350
-    INDIRECT_INFLUENCE_RADIUS = 15000
-    DECAY_CONSTANT = -0.0002
-    INDIRECT_INFLUENCE = 4
+    NOMINAL_ROAD_WIDTH = 300  # width of roads in inputs
+    DIRECT_INFLUENCE_WIDTH = 1000  # total width of direct influence (meters)
+    INDIRECT_INFLUENCE_RADIUS = (
+        15000  # distance from road indirect influence limit (meters)
+    )
+    DECAY_CONSTANT_MOTORIZED = -0.0003
+    DECAY_CONSTANT_NON_MOTORIZED = -0.0004
+    DIRECT_INDIRECT_INFLUENCE_RATIO = 0.5
 
     inputs = {
         "osm": {
@@ -30,40 +33,53 @@ class HIIRoad(HIITask):
             "ee_path": "projects/HII/v1/source/phys/watermask_jrc70_cciocean",
             "static": True,
         },
-    }
-    weights = {
-        "osm": {
-            "highway_residential": 10,
-            "highway_bridleway": 10,
-            "highway_bus_guideway": 10,
-            "highway_cycleway": 10,
-            "highway_elevator": 10,
-            "highway_escape": 4,
-            "highway_footway": 4,
-            "highway_living_street": 10,
-            "highway_mini_roundabout": 10,
-            "highway_motorway": 10,
-            "highway_motorway_link": 10,
-            "highway_path": 4,
-            "highway_pedestrian": 4,
-            "highway_primary": 10,
-            "highway_primary_link": 10,
-            "highway_raceway": 10,
-            "highway_rest_area": 10,
-            "highway_road": 10,
-            "highway_secondary": 10,
-            "highway_secondary_link": 10,
-            "highway_service": 10,
-            "highway_steps": 4,
-            "highway_tertiary": 10,
-            "highway_tertiary_link": 10,
-            "highway_track": 4,
-            "highway_trunk": 10,
-            "highway_trunk_link": 10,
-            "highway_turning_circle": 10,
-            "highway_unclassified": 10,
+        "ocean": {
+            "ee_type": HIITask.IMAGE,
+            "ee_path": "projects/HII/v1/source/phys/ESACCI-LC-L4-WB-Ocean-Map-150m-P13Y-2000-v40",
+            "static": True,
         },
-        "groad": 10,
+    }
+    # road keys and relative importance: https://wiki.openstreetmap.org/wiki/Key:highway
+    road_weights = {
+        "motorized": {
+            # roads
+            "highway_motorway": 10,
+            "highway_trunk": 10,
+            "highway_primary": 9,
+            "highway_secondary": 9,
+            "highway_tertiary": 8,
+            "highway_unclassified": 7,
+            "highway_residential": 7,
+            # link roads
+            "highway_motorway_link": 10,
+            "highway_trunk_link": 10,
+            "highway_primary_link": 9,
+            "highway_secondary_link": 9,
+            "highway_tertiary_link": 8,
+            # special road types
+            "highway_living_street": 7,
+            "highway_service": 8,
+            "highway_pedestrian": 6,
+            "highway_track": 5,
+            "highway_bus_guideway": 8,
+            "highway_escape": 8,
+            "highway_raceway": 8,
+            "highway_road": 10,
+            # other highway features
+            "highway_elevator": 8,
+            "highway_mini_roundabout": 8,
+            "highway_rest_area": 7,
+            "highway_turning_circle": 8,
+            # groad
+            "groad": 10,
+        },
+        "non_motorized": {
+            "highway_footway": 2,
+            "highway_bridleway": 3,
+            "highway_steps": 4,
+            "highway_path": 2,
+            "highway_cycleway": 4,
+        },
     }
 
     def __init__(self, *args, **kwargs):
@@ -77,50 +93,135 @@ class HIIRoad(HIITask):
         )
         self.groads = ee.Image(self.inputs["groads"]["ee_path"])
         self.water = ee.Image(self.inputs["water"]["ee_path"])
-        self.road_direct = None
+        self.ocean = ee.Image(self.inputs["ocean"]["ee_path"])
+        self.road_direct_cost = None
+        self.road_indirect_cost = None
 
-    def kernel(self):
-        return ee.Kernel.euclidean(radius=self.KERNEL_DISTANCE, units="meters")
+    def kernel(self, impact):
+        if impact == "DIRECT":
+            kernel_radius = self.DIRECT_INFLUENCE_WIDTH / 2
+        elif impact == "INDIRECT":
+            kernel_radius = self.INDIRECT_INFLUENCE_RADIUS
+        else:
+            kernel_radius = 0
+        return ee.Kernel.euclidean(radius=kernel_radius, units="meters")
 
     def osm_groads_combined_influence(self):
         osm_band_names = self.osm.bandNames()
-        road_band_names = list(self.weights["osm"].keys())
-        road_bands = osm_band_names.filter(ee.Filter.inList("item", road_band_names))
-        osm_roads = self.osm.select(road_bands)
-        weights_image = ee.Dictionary(self.weights["osm"]).toImage().select(road_bands)
+        motorized_weights = ee.Dictionary(self.road_weights["motorized"])
+        non_motorized_weights = ee.Dictionary(self.road_weights["non_motorized"])
+        road_weights = motorized_weights.combine(non_motorized_weights)
+        band_names = ee.Dictionary(
+            {
+                "all": road_weights.keys(),
+                "osm": osm_band_names.filter(
+                    ee.Filter.inList("item", road_weights.keys())
+                ),
+                "motorized": motorized_weights.keys().filter(
+                    ee.Filter.Or(
+                        ee.Filter.inList("item", osm_band_names),
+                        ee.Filter.eq("item", "groad"),
+                    )
+                ),
+                "non_motorized": non_motorized_weights.keys(),
+                "groad": ["groad"],
+            }
+        )
 
-        # creates 1km wide images with direct influence values
+        direct_weights = road_weights.toImage()
+        indriect_weights = direct_weights.multiply(self.DIRECT_INDIRECT_INFLUENCE_RATIO)
+
+        osm_roads = self.osm.select(band_names.get("osm"))
+
         osm_direct = (
-            osm_roads.distance(kernel=self.kernel(), skipMasked=False)
-            .lte(self.DIRECT_INFLUENCE_RADIUS)
-            .multiply(weights_image)
-        )
-        groads_direct = (
-            self.groads.distance(kernel=self.kernel(), skipMasked=False)
-            .lte(self.DIRECT_INFLUENCE_RADIUS)
-            .multiply(self.weights["groad"])
+            osm_roads.distance(kernel=self.kernel("DIRECT"), skipMasked=False)
+            .lte((self.DIRECT_INFLUENCE_WIDTH - self.NOMINAL_ROAD_WIDTH) / 2)
+            .multiply(direct_weights.select(band_names.get("osm")))
         )
 
-        # masks groads that overlap (< 500m) from an OSM road
-        non_osm_binary = osm_direct.reduce(ee.Reducer.anyNonZero()).unmask(0).eq(0)
-        groads_fill = groads_direct.updateMask(non_osm_binary)
+        groad_direct = (
+            self.groads.distance(kernel=self.kernel("DIRECT"), skipMasked=False)
+            .lte((self.DIRECT_INFLUENCE_WIDTH - self.NOMINAL_ROAD_WIDTH) / 2)
+            .multiply(direct_weights.select(band_names.get("groad")))
+        )
 
-        self.road_direct = (
-            osm_direct.addBands(groads_fill)
-            .reduce(ee.Reducer.sum())
-            .multiply(2)  # TODO: confirm reason and necessity of this step
+        non_osm = (
+            osm_direct.reduce(ee.Reducer.max())
             .unmask(0)
-            .rename("road_direct_influence")
+            .eq(0)
+            .updateMask(self.ocean)
+            .selfMask()
         )
+
+        groad_fill = groad_direct.updateMask(non_osm)
+
+        road_direct_bands = osm_direct.addBands(groad_fill)
+        self.road_direct_cost = road_direct_bands.reduce(ee.Reducer.max()).rename(
+            "road_direct"
+        )
+
+        road_indirect_cost_distance = road_direct_bands.distance(
+            kernel=self.kernel("INDIRECT"), skipMasked=False
+        )
+        motorized_indirect_cost = road_indirect_cost_distance.select(
+            band_names.get("motorized")
+        )
+        non_motorized_indirect_cost = road_indirect_cost_distance.select(
+            band_names.get("non_motorized")
+        )
+
+        motorized_decay = (
+            motorized_indirect_cost.multiply(self.DECAY_CONSTANT_MOTORIZED)
+            .exp()
+            .multiply(indriect_weights.select(band_names.get("motorized")))
+            .updateMask(
+                motorized_indirect_cost.lte(
+                    self.INDIRECT_INFLUENCE_RADIUS - (self.DIRECT_INFLUENCE_WIDTH / 2)
+                )
+            )
+            .reduce(ee.Reducer.max())
+            .rename("road_indirect_motorized")
+        )
+
+        non_motorized_decay = (
+            non_motorized_indirect_cost.multiply(self.DECAY_CONSTANT_NON_MOTORIZED)
+            .exp()
+            .multiply(indriect_weights.select(band_names.get("non_motorized")))
+            .updateMask(
+                non_motorized_indirect_cost.lte(
+                    self.INDIRECT_INFLUENCE_RADIUS - (self.DIRECT_INFLUENCE_WIDTH / 2)
+                )
+            )
+            .reduce(ee.Reducer.max())
+            .rename("road_indirect_non_motorized")
+        )
+        self.road_indirect_cost = motorized_decay.addBands(non_motorized_decay)
 
     def groads_influence(self):
-        self.road_direct = (
-            self.groads.distance(kernel=self.kernel(), skipMasked=False)
-            .lte(self.DIRECT_INFLUENCE_RADIUS)
-            .multiply(self.weights["groad"])
-            .multiply(2)  # TODO: confirm reason and necessity of this step
-            .unmask(0)
-            .rename("road_direct_influence")
+        groad_direct_weight = self.road_weights["motorized"]["groad"]
+        groad_indirect_weight = groad_direct_weight * (
+            self.DIRECT_INDIRECT_INFLUENCE_RATIO
+        )
+
+        self.road_direct_cost = (
+            self.groads.distance(kernel=self.kernel("DIRECT"), skipMasked=False)
+            .lte((self.DIRECT_INFLUENCE_WIDTH - self.NOMINAL_ROAD_WIDTH) / 2)
+            .multiply(groad_direct_weight)
+            .rename("road_direct")
+        )
+        groad_indirect_cost_distance = self.road_direct_cost.distance(
+            kernel=self.kernel("INDIRECT"), skipMasked=False
+        )
+        self.road_indirect_cost = (
+            groad_indirect_cost_distance.multiply(self.DECAY_CONSTANT_MOTORIZED)
+            .exp()
+            .multiply(groad_indirect_weight)
+            .updateMask(
+                groad_indirect_cost_distance.lte(
+                    self.INDIRECT_INFLUENCE_RADIUS - (self.DIRECT_INFLUENCE_WIDTH / 2)
+                )
+            )
+            .rename("road_indirect_motorized")
         )
 
     def calc(self):
@@ -128,25 +229,18 @@ class HIIRoad(HIITask):
             self.osm_groads_combined_influence()
         else:
             self.groads_influence()
-        road_binary = self.road_direct.gt(0).unmask(0)
-        road_indirect = (
-            road_binary.eq(0)
-            .cumulativeCost(road_binary, self.INDIRECT_INFLUENCE_RADIUS)
-            .multiply(self.DECAY_CONSTANT)
-            .exp()
-            .multiply(self.INDIRECT_INFLUENCE)
-            .reproject(crs=self.crs, scale=self.scale)
-            .unmask(0)
-            .rename("roads_indirect_influence")
-        )
-        # TODO: determine if drivers are to be normalized or not.
+
         road_driver = (
-            self.road_direct.where(self.road_direct.eq(0), road_indirect)
-            .rename("road_driver")
+            self.road_direct_cost.addBands(self.road_indirect_cost)
+            .reduce(ee.Reducer.max())
+            .unmask(0)
             .updateMask(self.water)
             .multiply(100)
             .int()
+            .rename("hii_road_driver")
         )
+
+        # # TODO: determine if drivers are to be normalized or not.
 
         self.export_image_ee(
             road_driver,
