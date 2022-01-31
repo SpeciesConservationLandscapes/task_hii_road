42063f6820ecc183cf616e5be0480d8171c14deb
Kim Fisher <kfisher@wcs.org>
2022-01-31 18:56:11 -0500

    another osm start date tweak

--- src/task.py
+++ src/task.py
@@ -5,7 +5,7 @@ from task_base import HIITask
 
 
 class HIIRoad(HIITask):
-    OSM_START = datetime(2014, 1, 1).date()
+    OSM_START = datetime(2014, 6, 4).date()
     NOMINAL_ROAD_WIDTH = 300  # width of roads in inputs
     DIRECT_INFLUENCE_WIDTH = 1000  # total width of direct influence (meters)
     INDIRECT_INFLUENCE_RADIUS = (
